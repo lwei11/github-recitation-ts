e8ff4e5c4105516c92b6f7e10af8ec574ff2453f
Neha Tirumalai <nehatirumalai@macbook-air-120.wifi.local.cmu.edu>
2023-08-30 13:36:26 -0400

    fix bug in fib.ts

--- src/fib.ts
+++ src/fib.ts
@@ -1,8 +1,8 @@
 // util function that computes the fibonacci numbers
-export default function fibonacci(n: number) {
+export default function fibonacci(n: number): number {
   if (n < 0) {
     return -1;
-  } else if (n == 0) {
+  } else if (n == 0) {  
     return 0;
   } else if (n == 1) {
     return 1;
